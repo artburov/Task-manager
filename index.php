4f69d808eb19b0abbfe6f913a88c7b8cdf162ac0
Arthur <burov@ukr.net>
2019-08-30 22:58:57 +0300

    added validation for empty Name and Message fields with color flash-notice

--- index.php
+++ index.php
@@ -82,51 +82,60 @@ $data = $statement -> fetchAll( PDO::FETCH_ASSOC );
                         <div class="card-header"><h3>Комментарии</h3></div>
 
                         <div class="card-body">
+                            <!-- Success flash message -->
                             <?php if ( isset( $_SESSION['message'] ) ) : ?>
                             <div class="alert alert-success" role="alert">
                                 <? echo $_SESSION['message'];
                                 endif;
                                 unset( $_SESSION['message'] ); ?>
-                            </div>
-                            <?php foreach ( $data as $comment ) : ?>
-                                <div class="media">
-                                    <img src="<?= $comment["image"]; ?>" class="mr-3" alt="..." width="64" height="64">
-                                    <div class="media-body">
-                                        <h5 class="mt-0"><?= $comment["user"]; ?></h5>
-                                        <span><small><?= $comment["date"]; ?></small></span>
-                                        <p>
-                                            <?= $comment["text"]; ?>
-                                        </p>
-                                    </div>
+                                <!-- Danger flash message -->
+                                <?php if ( isset ( $_SESSION['message_danger'] ) ) : ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <? echo $_SESSION['message_danger'];
+                                    endif;
+                                    unset( $_SESSION['message_danger'] ); ?>
                                 </div>
-                            <?php endforeach; ?>
+                                <!-- Loop shows all comments -->
+                                <?php foreach ( $data as $comment ) : ?>
+                                    <div class="media">
+                                        <img src="<?= $comment["image"]; ?>" class="mr-3" alt="..." width="64"
+                                             height="64">
+                                        <div class="media-body">
+                                            <h5 class="mt-0"><?= $comment["user"]; ?></h5>
+                                            <span><small><?= $comment["date"]; ?></small></span>
+                                            <p>
+                                                <?= $comment["text"]; ?>
+                                            </p>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="col-md-12" style="margin-top: 20px;">
-                    <div class="card">
-                        <div class="card-header"><h3>Оставить комментарий</h3></div>
+                    <div class="col-md-12" style="margin-top: 20px;">
+                        <div class="card">
+                            <div class="card-header"><h3>Оставить комментарий</h3></div>
 
-                        <div class="card-body">
-                            <form action="store.php" method="post">
-                                <div class="form-group">
-                                    <label for="exampleFormControlTextarea1">Имя</label>
-                                    <input name="name" class="form-control" id="exampleFormControlTextarea1"/>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlTextarea1">Сообщение</label>
-                                    <textarea name="text" class="form-control" id="exampleFormControlTextarea1"
-                                              rows="3"></textarea>
-                                </div>
-                                <button type="submit" class="btn btn-success">Отправить</button>
-                            </form>
+                            <div class="card-body">
+                                <form action="store.php" method="post">
+                                    <div class="form-group">
+                                        <label for="exampleFormControlTextarea1">Имя</label>
+                                        <input name="name" class="form-control" id="exampleFormControlTextarea1"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleFormControlTextarea1">Сообщение</label>
+                                        <textarea name="text" class="form-control" id="exampleFormControlTextarea1"
+                                                  rows="3"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-success">Отправить</button>
+                                </form>
+                            </div>
+
                         </div>
-
                     </div>
                 </div>
             </div>
-        </div>
     </main>
 </div>
 </body>
--- store.php
+++ store.php
@@ -2,12 +2,27 @@
 ini_set( 'error_reporting', E_ALL );
 session_start();
 
-$pdo = new PDO( "mysql:host=localhost; dbname=tasks", "root", "" );
-$sql = "INSERT INTO data (user, text, image, date) VALUES (:name, :text, 'img/no-user.jpg', date_format(current_date(), '%d/%m/%Y'))";
-$statement = $pdo -> prepare( $sql );
-$result = $statement -> execute( $_POST );
-if ($result == true) {
-    $_SESSION['message'] = 'Комментарий успешно добавлен!';
+$name = $_POST['name'];
+$text = $_POST['text'];
+
+if (empty( $name ) AND empty( $text )) {
+    $_SESSION['message_danger'] = 'Отсутствуют имя и сообщение';
+    goto end;
+} elseif (empty( $name )) {
+    $_SESSION['message_danger'] = 'Отсутствует имя';
+    goto end;
+} elseif (empty( $text )) {
+    $_SESSION['message_danger'] = 'Отсутствует сообщение';
+    goto end;
+} else {
+    $pdo = new PDO( "mysql:host=localhost; dbname=tasks", "root", "" );
+    $sql = "INSERT INTO data (user, text, image, date) VALUES (:name, :text, 'img/no-user.jpg', date_format(current_date(), '%d/%m/%Y'))";
+    $statement = $pdo -> prepare( $sql );
+    $result = $statement -> execute( $_POST );
+    if ($result == true) {
+        $_SESSION['message'] = 'Комментарий успешно добавлен';
+    }
 }
+end:
 
 header( "Location: /" );
