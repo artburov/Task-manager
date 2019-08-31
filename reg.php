9457c6d5e73200baf6ca83cc719b4fbb58d6301c
Arthur <burov@ukr.net>
2019-08-31 15:30:37 +0300

    added hashing for password field

--- reg.php
+++ reg.php
@@ -11,13 +11,15 @@ $password = $_POST['password'];
 $email = $_POST['email'];
 $email_confirm = $_POST['password_confirmation'];
 
+//Hashing user's password by default bcrypt algorithm
+$password_hash = password_hash( $password, PASSWORD_DEFAULT );
 
 $pdo = new PDO( "mysql:host=localhost; dbname=tasks", "root", "" );
 $sql = "INSERT INTO auth (user, password, email) VALUES (:name, :password, :email)";
 $statement = $pdo -> prepare( $sql );
-$statement->bindParam("name", $name);
-$statement->bindParam("password", $password);
-$statement->bindParam("email", $email);
+$statement -> bindParam( "name", $name );
+$statement -> bindParam( "password", $password_hash );
+$statement -> bindParam( "email", $email );
 
 $result = $statement -> execute();
 
